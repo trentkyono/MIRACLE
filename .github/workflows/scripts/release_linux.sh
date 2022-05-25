425296cc7e8ccebcb0b0c780bb8ba28e49690979
Bogdan Cebere <bogdan.cebere@gmail.com>
2022-05-25 09:39:40 +0100

    debug linux release

--- .github/workflows/scripts/release_linux.sh
+++ .github/workflows/scripts/release_linux.sh
@@ -2,6 +2,11 @@
 
 set -e
 
+yum makecache -y
+yum install centos-release-scl -y
+yum-config-manager --enable rhel-server-rhscl-7-rpms
+yum install llvm-toolset-7.0 python3 python3-devel -y
+
 # Python
 python3 -m pip install --upgrade pip
 python3 -m pip install setuptools wheel twine auditwheel
